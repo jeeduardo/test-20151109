b4076d880be65495903a35945e6903b411da2d99
jeeduardo <josephson.e.eduardo@accenture.com>
2015-11-09 12:29:49 +0900

    refs #10291: What does the fox say

--- fox.sh
+++ fox.sh
@@ -2,3 +2,5 @@
 
 echo "The quick brown fox jumps over the lazy dog. Oraaaaaaaayt!!!"
 # Will this work?
+echo "What does the fox say?"
+echo "Napoles: Di ko po alam. Di ko po matandaan. I invoke my right..."
